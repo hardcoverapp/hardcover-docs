27754bae78602fec24d6cb562aba58e2c50e10da
Kylie Benfield <kylie@revelryplay.com>
2024-08-26 18:59:07 -0600

    Changing colors just to show how easy it is to do

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,8 @@
-import colors from 'tailwindcss/colors';
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
 // Generated color palettes
-const accent = { 200: '#e3b6ed', 600: '#a700c3', 900: '#4e0e5b', 950: '#36113e' };
-const gray = { 100: '#f8f4fe', 200: '#f2e9fd', 300: '#c7bdd5', 400: '#9581ae', 500: '#614e78', 700: '#412e55', 800: '#2f1c42', 900: '#1c1425' };
+const accent = { 200: '#b6c8f4', 600: '#3d5dda', 900: '#1d2d63', 950: '#172143' };
+const gray = { 100: '#f6f6f8', 200: '#ededf2', 300: '#c1c1c7', 400: '#898b95', 500: '#565761', 700: '#363741', 800: '#25262f', 900: '#17181c' };
 
 
 /** @type {import('tailwindcss').Config} */
