310ee212ee6ac13e1f1d6274be0802a8b596bf81
Kylie Benfield <kylie@revelryplay.com>
2024-08-28 18:50:18 -0600

    Refining initial configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,36 +2,37 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://hardcover.revelryplay.com',
-	integrations: [
-		starlight({
-			logo: {
-				src: './src/assets/hardcover.svg'
-			},
-			title: 'API Documentation',
-			social: {
-				github: 'https://github.com/RevelryPlay/hardcover-doc',
-				discord: 'https://discord.gg/edGpYN8ym8',
-				mastodon: 'https://mastodon.hardcover.app/@hardcover',
-				instagram: 'https://instagram.com/hardcover.app',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Request', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'API Reference',
-					autogenerate: { directory: 'api/GraphQL' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  site: 'https://hardcover.revelryplay.com',
+  favicon: './src/assets/hardcover.svg',
+  integrations: [starlight({
+    logo: {
+      src: './src/assets/hardcover.svg'
+    },
+    title: 'API Documentation',
+    social: {
+      github: 'https://github.com/RevelryPlay/hardcover-doc',
+      discord: 'https://discord.gg/edGpYN8ym8',
+      mastodon: 'https://mastodon.hardcover.app/@hardcover',
+      instagram: 'https://instagram.com/hardcover.app'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+        // Each item here is one entry in the navigation menu.
+        // { label: 'Example Request', slug: 'guides/example' },
+      ]
+    }, {
+      label: 'API Reference',
+      autogenerate: {
+        directory: 'api/GraphQL'
+      }
+    }],
+    customCss: ['./src/tailwind.css']
+  }), tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
