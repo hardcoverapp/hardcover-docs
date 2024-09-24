736107d47926b1b4115e2dab0d92a04fe590767f
Kylie Benfield <kylie@revelryplay.com>
2024-09-23 23:03:10 -0600

    Removing unused screen import

--- src/components/GraphQLExplorer/StatusMessages.test.jsx
+++ src/components/GraphQLExplorer/StatusMessages.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import {StatusMessages} from "./StatusMessages";
 import {expect, it, describe} from "vitest";
 
