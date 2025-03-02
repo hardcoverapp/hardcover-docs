eecbed6b14774cbcbbba0597d7c1cdd6d6fe09d6
Kylie Benfield <kylie@revelryplay.com>
2025-03-02 10:44:55 -0600

    Adding initial ui.json files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ import react from "@astrojs/react";
 
 import { URLS } from './src/Consts';
 
+import * as EN from './src/content/docs/ui.json';
+import * as ES from './src/content/docs/es/ui.json';
+import * as FR from './src/content/docs/fr/ui.json';
+import * as IT from './src/content/docs/it/ui.json';
+import * as PL from './src/content/docs/pl/ui.json';
+
 // https://astro.build/config
 export default defineConfig({
     favicon: './src/assets/hardcover.svg',
@@ -21,24 +27,24 @@ export default defineConfig({
         lastUpdated: true,
         locales: {
             'es': {
-                label: 'Español',
-                lang: 'es'
+                label: ES.lang.label,
+                lang: ES.lang.code
             },
             'fr': {
-                 label: 'Français',
-                 lang: 'fr'
+                 label: FR.lang.label,
+                 lang: FR.lang.code
              },
             'it': {
-                label: 'Italiano',
-                lang: 'it'
+                label: IT.lang.label,
+                lang: IT.lang.code
             },
             'pl': {
-                label: 'Polski',
-                lang: 'pl'
+                label: PL.lang.label,
+                lang: PL.lang.code
             },
             root: {
-                label: 'English',
-                lang: 'en'
+                label: EN.lang.label,
+                lang: EN.lang.code
             },
         },
         logo: {
@@ -46,36 +52,37 @@ export default defineConfig({
         },
         sidebar: [
             {
-                label: 'API Docs',
+                label: EN.sidebar.api.title,
                 collapsed: true,
                 items: [
                     {
+                        label: EN.sidebar.api.gettingStarted,
                         slug: 'api/getting-started',
                         translations: {
-                            es: 'Empezando',
-                            fr: 'Commencer',
-                            it: 'Iniziare',
-                            pl: 'Zaczynając'
+                            es: ES.sidebar.api.gettingStarted,
+                            fr: FR.sidebar.api.gettingStarted,
+                            it: IT.sidebar.api.gettingStarted,
+                            pl: PL.sidebar.api.gettingStarted
                         }
                     },
                     {
-                        label: 'Guides',
+                        label: EN.sidebar.api.guides,
                         translations: {
-                            es: 'Guías',
-                            fr: 'Guides',
-                            it: 'Guide',
-                            pl: 'Przewodniki'
+                            es: ES.sidebar.api.guides,
+                            fr: FR.sidebar.api.guides,
+                            it: IT.sidebar.api.guides,
+                            pl: PL.sidebar.api.guides
                         },
                         autogenerate: {directory: 'api/guides'},
                         collapsed: true,
                     },
                     {
-                        label: 'Schemas',
+                        label: EN.sidebar.api.schemas,
                         translations: {
-                            es: 'Esquemas',
-                            fr: 'Schémas',
-                            it: 'Schemi',
-                            pl: 'Schematy'
+                            es: ES.sidebar.api.schemas,
+                            fr: FR.sidebar.api.schemas,
+                            it: IT.sidebar.api.schemas,
+                            pl: PL.sidebar.api.schemas
                         },
                         autogenerate: {directory: 'api/GraphQL/Schemas'},
                         collapsed: true,
@@ -83,25 +90,116 @@ export default defineConfig({
                 ]
             },
             {
-                label: 'Contributing Guides',
+                label: EN.sidebar.contributing.title,
                 collapsed: true,
-                autogenerate: {directory: 'contributing'},
+                items: [
+                    {
+                        label: EN.sidebar.contributing.api,
+                        slug: 'contributing/api-docs',
+
+                        translations: {
+                            es: ES.sidebar.contributing.api,
+                            fr: FR.sidebar.contributing.api,
+                            it: IT.sidebar.contributing.api,
+                            pl: PL.sidebar.contributing.api
+                        }
+                    },
+                    {
+                        label: EN.sidebar.contributing.librarian,
+                        slug: 'contributing/librarian-guides',
+
+                        translations: {
+                            es: ES.sidebar.contributing.librarian,
+                            fr: FR.sidebar.contributing.librarian,
+                            it: IT.sidebar.contributing.librarian,
+                            pl: PL.sidebar.contributing.librarian
+                        }
+                    },
+                    {
+                        label: EN.sidebar.contributing.translations,
+                        slug: 'contributing/doc-translations',
+
+                        translations: {
+                            es: ES.sidebar.contributing.translations,
+                            fr: FR.sidebar.contributing.translations,
+                            it: IT.sidebar.contributing.translations,
+                            pl: PL.sidebar.contributing.translations
+                        }
+                    }
+                ],
                 translations: {
-                    es: 'Guías de contribución',
-                    fr: 'Guides de contribution',
-                    it: 'Guide alla contribuzione',
-                    pl: 'Przewodniki dla współtwórców'
+                    es: ES.sidebar.contributing.title,
+                    fr: FR.sidebar.contributing.title,
+                    it: IT.sidebar.contributing.title,
+                    pl: PL.sidebar.contributing.title
                 }
             },
             {
-                label: 'Librarian Guides',
+                label: EN.sidebar.librarians.title,
                 collapsed: true,
-                autogenerate: {directory: 'librarians'},
+                items: [
+                    {
+                        label: EN.sidebar.librarians.editing,
+                        slug: 'librarians/editing',
+
+                        translations: {
+                            es: ES.sidebar.librarians.editing,
+                            fr: FR.sidebar.librarians.editing,
+                            it: IT.sidebar.librarians.editing,
+                            pl: PL.sidebar.librarians.editing
+                        }
+                    },
+                    {
+                        label: EN.sidebar.librarians.faq,
+                        slug: 'librarians/faq',
+
+                        translations: {
+                            es: ES.sidebar.librarians.faq,
+                            fr: FR.sidebar.librarians.faq,
+                            it: IT.sidebar.librarians.faq,
+                            pl: PL.sidebar.librarians.faq
+                        }
+                    },
+                    // {
+                    //     label: EN.sidebar.librarians.gettingStarted,
+                    //     slug: 'librarians/getting-started',
+                    //
+                    //     translations: {
+                    //         es: ES.sidebar.librarians.gettingStarted,
+                    //         fr: FR.sidebar.librarians.gettingStarted,
+                    //         it: IT.sidebar.librarians.gettingStarted,
+                    //         pl: PL.sidebar.librarians.gettingStarted
+                    //     }
+                    // }
+                    {
+                        label: EN.sidebar.librarians.resources,
+                        autogenerate: {directory: 'librarians/Resources'},
+
+                        translations: {
+                            es: ES.sidebar.librarians.resources,
+                            fr: FR.sidebar.librarians.resources,
+                            it: IT.sidebar.librarians.resources,
+                            pl: PL.sidebar.librarians.resources
+                        }
+                    },
+                    {
+                        label: EN.sidebar.librarians.standards,
+                        autogenerate: {directory: 'librarians/Standards'},
+
+                        translations: {
+                            es: ES.sidebar.librarians.standards,
+                            fr: FR.sidebar.librarians.standards,
+                            it: IT.sidebar.librarians.standards,
+                            pl: PL.sidebar.librarians.standards
+                        }
+                    }
+                ],
+
                 translations: {
-                    es: 'Guías del bibliotecario',
-                    fr: 'Guides du bibliothécaire',
-                    it: 'Guide del bibliotecario',
-                    pl: 'Przewodniki bibliotekarza'
+                    es: ES.sidebar.librarians.title,
+                    fr: FR.sidebar.librarians.title,
+                    it: IT.sidebar.librarians.title,
+                    pl: PL.sidebar.librarians.title
                 }
             }
         ],
@@ -112,11 +210,7 @@ export default defineConfig({
             mastodon: URLS.MASTODON,
         },
         title: {
-            en: 'Hardcover',
-            es: 'Hardcover',
-            fr: 'Hardcover',
-            it: 'Hardcover',
-            pl: 'Hardcover'
+            en: EN.site.title,
         }
     }), tailwind({
         applyBaseStyles: false
