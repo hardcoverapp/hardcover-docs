e25f4982d5535803463d34d35f0f7303082f877c
Kylie Benfield <kylie@revelryplay.com>
2025-03-05 19:26:49 -0600

    Renaming translation keys

--- src/components/LibrarianBanners.tsx
+++ src/components/LibrarianBanners.tsx
@@ -12,7 +12,7 @@ export const LibrarianBanners = (lang: any = "en") => {
     // const isResources = currentPath.includes('/resources/');
 
     // @ts-ignore
-    const bannerText: string | Node = useTokenTranslation('pages.librarians.standards.bannerText', locale, {
+    const bannerText: string | Node = useTokenTranslation('pages.librarians.standardsBanner.text', locale, {
         "a": () => {
             return `<a href=${URLS.LIBRARIAN_DISCORD}
                                            target="_blank" rel="noreferrer noopener">{chunks}</a>`
@@ -28,7 +28,7 @@ export const LibrarianBanners = (lang: any = "en") => {
             {isStandards && (
                 <div className="border-l-4 border-l-accent-600 bg-accent-200 dark:bg-accent-950 p-4 dark:text-white">
                     <h5 className="!text-accent-900 dark:!text-accent-200">{
-                        useTranslation('pages.librarians.standards.bannerTitle', locale)
+                        useTranslation('pages.librarians.standardsBanner.title', locale)
                     }:</h5>
                     <p dangerouslySetInnerHTML={sanitizedBannerText()}/>
                 </div>
