164133b76bd6dabed9d3a4fbb13658a87b32043c
zivoy <ziv132@gmail.com>
2026-08-15 21:06:21 -0400

    correctish

--- src/examples/test/get_books.py
+++ src/examples/test/get_books.py
@@ -1,14 +1,14 @@
-from hardcover-sdk import HardcoverClientt
+from hardcover_sdk import HardcoverClient
 import os
 
 # SECTION START: setup :SECTION
-client = HardcoverClient(apiKey=os.environ["HARDCOVER_API_KEY"]);
+client = HardcoverClient(apiKey=os.environ["HARDCOVER_API_KEY"])
 # SECTION END: setup :SECTION
 
 # SECTION START: fetchBooks :SECTION
-books = client.query(`
+books = client.query("""
 	query { books(limit: 5) { id title } }
-`);
+""")
 # SECTION END: fetchBooks :SECTION
 
-print(books);
+print(books)
