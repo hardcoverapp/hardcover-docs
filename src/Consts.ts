7bee00061726d5e1c837e0308e678003c9288294
Adam Fortuna <adamfortuna@users.noreply.github.com>
2026-08-20 10:29:57 -0600

    Feature/api jwt to pat (#168)
    * Updated 'Getting an API Key' for PAT method
    
    * Updated docs for new API token generation
    
    * copy over images from zivoy:new-auth-docs
    
    * new api key link
    
    * add getting started updated
    
    * simplify
    
    * drop reference to oauth
    
    * explain rate limit headers
    
    * add a warning about halved burst capacity
    
    * force newlines
    
    * explain token bucket
    
    * fix incorrect description of burst limit
    
    * update message for 403
    
    * correct wording
    
    * redo response table and expand on 403 shapes
    
    * keep the option open
    
    * Fix date
    
    ---------
    
    Co-authored-by: zivoy <ziv132@gmail.com>

--- src/Consts.ts
+++ src/Consts.ts
@@ -2,7 +2,10 @@ export const URLS = {
     APP: 'https://hardcover.app',
     API: 'https://api.hardcover.app',
     API_ACCOUNT_URL: 'https://hardcover.app/account/api',
+    API_NEW_TOKEN_URL: 'https://hardcover.app/account/api/keys/new',
     DOCS: 'https://docs.hardcover.app',
+    POLICIES: 'https://hardcover.app/pages/policies',
+    DMCA: 'https://hardcover.app/pages/dmca',
 
     GRAPHQL_URL: 'https://api.hardcover.app/v1/graphql',
 
@@ -38,4 +41,8 @@ export const defaultPreferences: {
     theme: 'auto',
     editMode: 'basic',
     graphQLResults: 'table',
+}
+
+export const EMAILS = {
+    SUPPORT: 'jules@hardcover.app'
 }
